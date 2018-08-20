d84e54f1868ec0a19d6be5a6bcd4833d657b5d7b
Bernhard Rosenkränzer <bero@lindev.ch>
2018-08-20 21:19:41 +0200

    Assorted fixes

--- 3.x-to-cooker.sh
+++ 3.x-to-cooker.sh
@@ -15,9 +15,15 @@ if ! [ -d "$TMPDIR" ]; then
 fi
 cd "$TMPDIR"
 
-urpmi -y --auto --auto-update
-urpmi --auto curl wget
+# Update 3.x branch and get it into shape...
+urpmi --auto --auto-update
+urpmi --auto curl wget db52-utils
 
+# Get rid of stuff that will be replaced with alternatives
+# that have different names...
+urpme -a --auto --force perl-URPM perl-RPMBDB perl-MDV-Packdrakeng perl-MDV-Distribconf gurpmi genhdlist2
+
+# Save list of installed packages for recovery purposes...
 rpm -qa --qf '%{NAME}\n' >package.list
 
 ARCH="$(uname -m)"
@@ -30,7 +36,7 @@ else
 fi
 PKGS=http://abf-downloads.openmandriva.org/cooker/repository/$ARCH/main/release/
 curl -s -L $PKGS |grep '^<a' |cut -d'"' -f2 >PACKAGES
-PACKAGES="deltarpm distro-release-OpenMandriva distro-release-common dnf dnf-automatic dnf-conf dnf-yum dwz hawkey-man glibc ${LIB}comps0 ${LIB}createrepo_c0 ${LIB}crypto1.1 ${LIB}ssl1.1 ${LIB}curl4 ${LIB}db6.2 ${LIB}dnf2 ${LIB}idn2_0 ${LIB}gpgme11 ${LIB}gpgmepp6 ${LIB}repo0 ${LIB}rpm8 ${LIB}rpmbuild8 ${LIB}rpmsign8 ${LIB}solv0 ${LIB}solvext0 ${LIB}zstd1 ${LIB}lua5.3 libsolv openmandriva-repos openmandriva-repos-cooker openmandriva-repos-keys openmandriva-repos-pkgprefs ${LIB}python3.7m_1 ${LIB}stdc%2B%2B6 ${LIB}json-c4 ${LIB}yaml0_2 ${LIB}crypt1 python python-dnf python-dnf-plugin-leaves python-dnf-plugin-show-leaves python-dnf-plugin-versionlock python-dnf-plugins-core python-libdnf python-gi python-smartcols ${LIB}modulemd-gir1.0 ${LIB}modulemd1 ${LIB}glib2.0_0 ${LIB}gobject2.0_0 ${LIB}girepository1.0_1 ${LIB}glib-gir2.0 python-gpg python-hawkey python-iniparse python-libcomps python-librepo python-rpm python-six rpm rpm-openmandriva-setup rpm-plugin-ima rpm-plugin-syslog rpm-plugin-systemd-inhibit rpm-sign rpmlint rpmlint-distro-policy"
+PACKAGES="deltarpm distro-release-OpenMandriva distro-release-common dnf dnf-automatic dnf-conf dnf-yum dwz hawkey-man glibc ${LIB}comps0 ${LIB}createrepo_c0 ${LIB}crypto1.1 ${LIB}ssl1.1 ${LIB}curl4 ${LIB}db6.2 ${LIB}dnf2 ${LIB}idn2_0 ${LIB}gpgme11 ${LIB}gpgmepp6 ${LIB}repo0 ${LIB}rpm8 ${LIB}rpmbuild8 ${LIB}rpmsign8 ${LIB}solv0 ${LIB}solvext0 ${LIB}zstd1 ${LIB}lua5.3 libsolv openmandriva-repos openmandriva-repos-cooker openmandriva-repos-keys openmandriva-repos-pkgprefs ${LIB}python3.7m_1 ${LIB}stdc%2B%2B6 ${LIB}json-c4 ${LIB}yaml0_2 ${LIB}crypt1 python python-dnf python-dnf-plugin-leaves python-dnf-plugin-show-leaves python-dnf-plugin-versionlock python-dnf-plugins-core python-libdnf python-gi python-smartcols ${LIB}modulemd-gir1.0 ${LIB}modulemd1 ${LIB}glib2.0_0 ${LIB}gobject2.0_0 ${LIB}girepository1.0_1 ${LIB}glib-gir2.0 python-gpg python-hawkey python-iniparse python-libcomps python-librepo python-rpm python-six rpm rpm-openmandriva-setup rpm-plugin-ima rpm-plugin-syslog rpm-plugin-systemd-inhibit rpm-sign rpmlint rpmlint-distro-policy db62-utils"
 for i in $PACKAGES; do
 	P=`grep "^$i-[0-9].*" PACKAGES`
 	if [ "$?" != "0" ]; then
@@ -40,6 +46,14 @@ for i in $PACKAGES; do
 	wget $PKGS/$P
 done
 
+cd /var/lib/rpm
+mkdir -p /var/lib/RPMNEW
+cp Packages /var/lib/RPMNEW/
+mv alternatives /var/lib/RPMNEW/
+mv filetriggers /var/lib/RPMNEW/
+db52_dump Packages >../RPMNEW/Packages.dump
+
+cd "$TMPDIR"
 # --oldpackage is to allow rpm to go from 5.x to 4.x without forcing an Epoch
 # --force shouldn't be needed, but is there just in case someone manually
 # installed a relevant cooker package before. That shouldn't make the rpm
@@ -53,38 +67,46 @@ done
 # fulfilled.
 # So for now, update the minimal set of required packages and let dnf
 # handle the rest.
-cd /var/lib/rpm
-mkdir -p /var/lib/RPMNEW
-cp ./Packages /var/lib/RPMNEW/
-mv alternatives /var/lib/RPMNEW/
-mv filetriggers /var/lib/RPMNEW/
-cd "$TMPDIR"
-# As soon as this is run the rpmdb will only contain the packages that are in the $TMPDIR
-# Thus you end up with a db of about 10mB so dnf doesn't want to upgrad anything because there's very little there.
 rpm -Uvh --force --oldpackage --nodeps *.rpm
 # After installing the necessary packages let's restore the original db because now we have the necessary libraries to convert it properly
-cp /var/lib/RPMNEW/Packages /var/lib/rpm/
-cd -
-# Rebuild the rpmdb twice, first attempt likely fails but corrects the failure,
-# enabling the second run to succeed
-rpm --rebuilddb
-rpm --rebuilddb
+cd /var/lib/rpm
+tries=0
+while [ "$tries" -lt 10 ]; do
+	# There is some weird issue with rpm --rebuilddb
+	# sometimes, but not always, wiping the rpmdb when
+	# the Packages file comes from rpm 5.
+	# Let's try until we're lucky.
+	tries=$((tries+1)
+	db62_load Packages <../RPMNEW/Packages.dump
+	# Rebuild the rpmdb twice, first attempt likely fails but corrects the failure,
+	# enabling the second run to succeed
+	rpm --rebuilddb
+	rpm --rebuilddb
+	[ "$(rpm -qa |wc -l)" -ge 25 ] && break
+done
+if [ "$tries" = 10 ]; then
+	echo "Restoring the rpm database after switching to rpm4 failed."
+	echo "Please find the developers on #openmandriva-cooker on freenode."
+	exit 1
+fi
+
 # Now we have a good db move back some useful files
 mv /var/lib/RPMNEW/alternatives /var/lib/rpm/
 mv /var/lib/RPMNEW/filetriggers /var/lib/rpm/
 cd /var/lib
 # Remove the backup directory
-/bin/rm -R /var/lib/RPMNEW
+/bin/rm -rf /var/lib/RPMNEW
 # Rebuild the rpmdb again
 rpm --rebuilddb
+rpm --rebuilddb
+
 # This bit is important. Re-install the packages that we installed to allow us to update
 # Now the db should be fully current
 cd "$TMPDIR"
 rpm -Uvh --justdb --force --oldpackage --nodeps *.rpm
 
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-OpenMandriva
-cp /etc/shadow /etc/gshadow /etc/passwd /etc/group .
-dnf -y erase perl-URPM perl-RPMBDB perl-MDV-Packdrakeng perl-MDV-Distribconf gurpmi genhdlist2
+cp -f /etc/shadow /etc/gshadow /etc/passwd /etc/group .
 # Workaround for dnf transaction error on perl-base, don't worry, perl
 # automatically gets reinstalled by dnf (as a dependency of packages
 # that are being updated)
